f186124fdaa3ae8a07533a602babfa17bfb2d259
Trey Pendragon <tpendragon@princeton.edu>
2019-12-04 10:57:59 -0800

    Start generating documents.

--- test/digital_collex/indexer_test.exs
+++ test/digital_collex/indexer_test.exs
@@ -14,4 +14,9 @@ defmodule DigitalCollex.IndexerTest do
     assert %{"docs" => [doc | _]} = response
     assert %{"id" => "4c4bd924-70c5-4e1c-8bc5-2ad06a995ccc"} = doc
   end
+
+  test "converts catalog data to elasticsearch documents" do
+    %{"response" => %{"docs" => documents}} = Indexer.get_figgy_catalog(MockedGetter)
+    output = Indexer.convert_figgy_documents(documents)
+  end
 end
