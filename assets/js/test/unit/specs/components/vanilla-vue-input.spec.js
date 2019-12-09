f2c1dcdae4819ae02a41a5d8e46f05d6ac318901
GriffinJ <jrgriffiniii@gmail.com>
2019-12-09 10:14:53 -0500

    Adding Prettier as a dev. dependency

--- assets/js/test/unit/specs/components/vanilla-vue-input.spec.js
+++ assets/js/test/unit/specs/components/vanilla-vue-input.spec.js
@@ -1,10 +1,10 @@
-import { createLocalVue, mount } from "@vue/test-utils"
-import VanillaVueInput from '@/components/VanillaVueInput.vue'
+import { createLocalVue, mount } from "@vue/test-utils";
+import VanillaVueInput from "@/components/VanillaVueInput.vue";
 
-const localVue = createLocalVue()
+const localVue = createLocalVue();
 
-describe('VanillaVueInput', () => {
-  let wrapper
+describe("VanillaVueInput", () => {
+  let wrapper;
 
   beforeEach(() => {
     wrapper = mount(VanillaVueInput, {
@@ -12,10 +12,12 @@ describe('VanillaVueInput', () => {
       props: {
         id: "Foo"
       }
-    })
-  })
+    });
+  });
 
-  it('renders the correct markup', () => {
-    expect(wrapper.html()).toContain("<form>\n  <fieldset><input type=\"text\" placeholder=\"Type a value here...\"></fieldset>\n  <p>Value: </p>\n</form>")
-  })
-})
+  it("renders the correct markup", () => {
+    expect(wrapper.html()).toContain(
+      '<form>\n  <fieldset><input type="text" placeholder="Type a value here..."></fieldset>\n  <p>Value: </p>\n</form>'
+    );
+  });
+});
