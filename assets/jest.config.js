27c0483669ebf56382dd8369a7862cf914401870
GriffinJ <jrgriffiniii@gmail.com>
2019-12-09 12:04:20 -0500

    Moving the configuration back to a jest.config.js

---
+++ assets/jest.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  modulePaths: ["<rootDir>"],
+  moduleFileExtensions: ["js", "json", "vue"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/js/$1"
+  },
+  transform: {
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+  },
+  coverageDirectory: "<rootDir>/js/test/unit/coverage",
+  collectCoverageFrom: [
+    "<rootDir>/js/components/**/*.{js,vue}",
+    "!<rootDir>/js/components/_components.js",
+    "!<rootDir>/js/components/_lux-elements-used.js",
+    "!<rootDir>/node_modules/**"
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  }
+};
