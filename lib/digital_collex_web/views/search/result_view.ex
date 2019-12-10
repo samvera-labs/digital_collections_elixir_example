3f452f66a98301265dd787941f61493917c989d9
GriffinJ <jrgriffiniii@gmail.com>
2019-12-10 12:03:32 -0500

    Use a LUX element for a facet dropdown on the search results template
    Co-authored-by: Anna Headley <anna.headley@gmail.com>
    Co-authored-by: Christina Chortaria <actspatial@gmail.com>
    Co-authored-by: Eliot Jordan <eliotj@princeton.edu>
    Co-authored-by: Trey Pendragon <tpendragon@princeton.edu>

--- lib/digital_collex_web/views/search/result_view.ex
+++ lib/digital_collex_web/views/search/result_view.ex
@@ -1,3 +1,9 @@
 defmodule DigitalCollexWeb.Search.ResultView do
   use DigitalCollexWeb, :view
+
+  def lux_facet_options(label, buckets) do
+    Enum.map(buckets, fn({ name, count } ) -> %{ label: "#{name}: #{count}", value: name } end )
+    |> List.insert_at(0, %{label: "Filter by #{label}", value: ""})
+    |> Jason.encode!
+  end
 end
