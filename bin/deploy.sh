ec0ad1f5f3c271f5fd5366d3ff776f7e3b06b89a
christinach <actspatial@gmail.com>
2019-12-12 14:17:46 -0500

    Untar and run migrations

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -14,6 +14,8 @@ function deploy() {
   VERSION=`awk '/@version \"(.*)\"/{ print $2 }' ./mix.exs | cut -d '"' -f2`
   docker run -v $(pwd):/build -w /build -e MIX_ENV=$DEPLOY_ENV -ti samvera/elixir-build bash -c "mix deps.get && mix release --overwrite"
   scp _build/${DEPLOY_ENV}/${APP}-${VERSION}.tar.gz ${DEPLOY_USER}@${DEPLOY_SERVER}:${DEPLOY_DIR}
+  ssh ${DEPLOY_USER}@${DEPLOY_SERVER} -- "cd ${DEPLOY_DIR} && tar -xzf ${APP}-${VERSION}.tar.gz"
+  ssh ${DEPLOY_USER}@${DEPLOY_SERVER} -- "cd ${DEPLOY_DIR} && ./bin/digital_collex eval 'DigitalCollex.ReleaseTasks.migrate()'"
 }
 
 deploy
